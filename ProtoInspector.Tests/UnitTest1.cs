de879f615ece18974a4c47365462cd8496d06df4
Roland Griesser <roland.griesser@bitmovin.com>
2021-02-16 11:04:00 -0500

    Added Google dep

--- ProtoInspector.Tests/UnitTest1.cs
+++ ProtoInspector.Tests/UnitTest1.cs
@@ -16,7 +16,6 @@ namespace ProtoInspector.Tests
 {
     public class UnitTest1
     {
-        static Action<string> Write = Console.WriteLine;
         /// <summary>
         /// 
         /// </summary>https://laurentkempe.com/2019/02/18/dynamically-compile-and-run-code-using-dotNET-Core-3.0/
@@ -27,79 +26,42 @@ namespace ProtoInspector.Tests
             // var proto = System.IO.File.ReadAllText("../../../person.proto");
             // System.IO.File.WriteAllText("temp.proto", proto);
             // Process.Start("protoc --csharp_out=. temp.proto");
-            // var provider = CodeDomProvider.CreateProvider("CSharp");
-            // var options = new CompilerParameters();
-            // var assembly = provider.CompileAssemblyFromFile(options, "../../../Person.cs");
 
-            //             string source =
-            //                         @"
-            // namespace Foo
+            var codeToCompile = System.IO.File.ReadAllText("../../../Person.cs");
+            // string codeToCompile = @"
+            // using System;
+            // namespace RoslynCompileSample
             // {
-            //     public class Bar
+            //     public class Person
             //     {
-            //         public void SayHello()
-            //         {
-            //             System.Console.WriteLine(""Hello World"");
-            //         }
+            //         public string Name  { get; set; }
             //     }
-            // }
-            //             ";
+            // }";
 
-            //             Dictionary<string, string> providerOptions = new Dictionary<string, string>
-            //                 {
-            //                     {"CompilerVersion", "v3.5"}
-            //                 };
-            //             CSharpCodeProvider provider = new CSharpCodeProvider(providerOptions);
+            var syntaxTree = CSharpSyntaxTree.ParseText(codeToCompile);
 
-            //             CompilerParameters compilerParams = new CompilerParameters
-            //             {
-            //                 GenerateInMemory = true,
-            //                 GenerateExecutable = false
-            //             };
+            var assemblyName = Path.GetRandomFileName();
+            var dd = typeof(Enumerable).GetTypeInfo().Assembly.Location;
+            var coreDir = Directory.GetParent(dd);
 
-            //             CompilerResults results = provider.CompileAssemblyFromSource(compilerParams, source);
+            // MetadataReference.CreateFromFile(coreDir.FullName + Path.DirectorySeparatorChar + "netstandard.dll")
 
-            //             if (results.Errors.Count != 0)
-            //                 throw new Exception("Mission failed!");
-
-            //             object o = results.CompiledAssembly.CreateInstance("Foo.Bar");
-            //             MethodInfo mi = o.GetType().GetMethod("SayHello");
-            //             mi.Invoke(o, null);
-
-            //             System.Console.WriteLine("done");
-
-            Write("Let's compile!");
-
-            string codeToCompile = @"
-            using System;
-            namespace RoslynCompileSample
-            {
-                public class Writer
-                {
-                    public void Write(string message)
-                    {
-                        Console.WriteLine($""you said '{message}!'"");
-                    }
-                }
-            }";
-
-            Write("Parsing the code into the SyntaxTree");
-            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(codeToCompile);
-
-            string assemblyName = Path.GetRandomFileName();
             var refPaths = new[] {
                 typeof(System.Object).GetTypeInfo().Assembly.Location,
+                typeof(Google.Protobuf.IMessage).GetTypeInfo().Assembly.Location,
                 typeof(Console).GetTypeInfo().Assembly.Location,
+                typeof(IEquatable<>).GetTypeInfo().Assembly.Location,
+                coreDir.FullName + Path.DirectorySeparatorChar + "netstandard.dll",
                 Path.Combine(Path.GetDirectoryName(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location), "System.Runtime.dll")
             };
-            MetadataReference[] references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
+            var references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
 
-            Write("Adding the following references");
-            foreach (var r in refPaths)
-                Write(r);
+            // Write("Adding the following references");
+            // foreach (var r in refPaths)
+            //     Write(r);
 
-            Write("Compiling ...");
-            CSharpCompilation compilation = CSharpCompilation.Create(
+            // Write("Compiling ...");
+            var compilation = CSharpCompilation.Create(
                 assemblyName,
                 syntaxTrees: new[] { syntaxTree },
                 references: references,
@@ -111,7 +73,6 @@ namespace ProtoInspector.Tests
 
                 if (!result.Success)
                 {
-                    Write("Compilation failed!");
                     IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
                         diagnostic.IsWarningAsError ||
                         diagnostic.Severity == DiagnosticSeverity.Error);
@@ -123,14 +84,18 @@ namespace ProtoInspector.Tests
                 }
                 else
                 {
-                    Write("Compilation successful! Now instantiating and executing the code ...");
                     ms.Seek(0, SeekOrigin.Begin);
 
                     Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
-                    var type = assembly.GetType("RoslynCompileSample.Writer");
-                    var instance = assembly.CreateInstance("RoslynCompileSample.Writer");
-                    var meth = type.GetMember("Write").First() as MethodInfo;
-                    meth.Invoke(instance, new[] { "joel" });
+                    var types = assembly.GetTypes();
+
+                    JsonSerializer.
+                    // var exportedTypes = assembly.GetExportedTypes();
+                    // var forwardedTypes = assembly.GetForwardedTypes();
+                    // var type = assembly.GetType("RoslynCompileSample.Writer");
+                    // var instance = assembly.CreateInstance("RoslynCompileSample.Writer");
+                    // var meth = type.GetMember("Write").First() as MethodInfo;
+                    // meth.Invoke(instance, new[] { "joel" });
                 }
             }
         }
