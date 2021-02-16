e6258a5df0ebabe80e284db906fb0c0a18b0d096
Roland Griesser <roland.griesser@bitmovin.com>
2021-02-16 14:25:21 -0500

    Create HexHelpers.cs

---
+++ ProtoInspector/Helpers/HexHelpers.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System;
+
+namespace ProtoInspector.Helpers
+{
+    public static class HexHelpers
+    {
+        public static string ToHexString(this byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "");
+        }
+
+        public static byte[] HexToByteArray(this string hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+    }
+}
