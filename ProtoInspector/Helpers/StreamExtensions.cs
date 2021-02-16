c7e18f6be3d618b8e6f225606e9c581276b26036
Roland Griesser <roland.griesser@bitmovin.com>
2021-02-16 12:02:53 -0500

    Added helper functions for hex conversion

---
+++ ProtoInspector/Helpers/StreamExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ProtoInspector.Helpers
+{
+    public static class StreamExtensions
+    {
+        public static string ToHexString(this byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "");
+        }
+
+        public static byte[] HexToByteArray(this string hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+    }
+}
