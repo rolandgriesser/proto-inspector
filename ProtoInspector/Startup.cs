c6577cdb2d79bbabcea169d3d2120137e7afc61d
Roland Griesser <roland.griesser@bitmovin.com>
2021-02-16 20:07:20 -0500

    Added Session

--- ProtoInspector/Startup.cs
+++ ProtoInspector/Startup.cs
@@ -23,6 +23,15 @@ namespace ProtoInspector
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+
             services.AddRazorPages()
                 .AddRazorRuntimeCompilation();
         }
@@ -47,6 +56,7 @@ namespace ProtoInspector
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
